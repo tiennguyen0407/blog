c07df3888983edf4a14e56c445ea229345da2bbf
tiennt <tiennt@firegroup.io>
2024-11-25 22:40:40 +0700

    test

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
+  website: "https://ezbuild.site/", // replace this with your deployed domain
+  author: "Tien Nguyen",
   profile: "https://satnaing.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "DevOps Journey",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
